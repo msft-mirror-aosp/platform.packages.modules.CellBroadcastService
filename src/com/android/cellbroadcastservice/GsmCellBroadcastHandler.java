5ebf6224092ca70ec8ef9f75dd46b94b79c7146c
Michael Groover <mpgroover@google.com>
2022-09-17 00:20:00 +0000

    Export receivers in GsmCellBroadcastHandler
    The runtime receivers in GsmCellBroadcastHandler are currently
    registered with the RECEIVER_NOT_EXPORTED flag; this prevents
    other apps from being able to send broadcasts to these receivers.
    However, these receivers are guarded by a signature permission,
    and broadcasts are expected from external apps that are signed
    with the same signing key. This commit registers these receivers
    with the RECEIVER_EXPORTED flag to allow the expected broadcasts
    from external apps that are signed with the same signing key and
    granted the signature permission that is guarding these receivers.
    
    Fixes: 245018067
    Test: atest GsmCellBroadcastHandlerTest
    Change-Id: I1ff3dd9deac6faa017b5f856311b1aaf686ba875

--- src/com/android/cellbroadcastservice/GsmCellBroadcastHandler.java
+++ src/com/android/cellbroadcastservice/GsmCellBroadcastHandler.java
@@ -127,7 +127,7 @@ public class GsmCellBroadcastHandler extends CellBroadcastHandler {
         super("GsmCellBroadcastHandler", context, looper, cbSendMessageCalculatorFactory,
                 handlerHelper);
         mContext.registerReceiver(mGsmReceiver, new IntentFilter(ACTION_AREA_UPDATE_ENABLED),
-                CBR_MODULE_PERMISSION, null, Context.RECEIVER_NOT_EXPORTED);
+                CBR_MODULE_PERMISSION, null, RECEIVER_EXPORTED);
         mContext.registerReceiver(mGsmReceiver,
                 new IntentFilter(SubscriptionManager.ACTION_DEFAULT_SUBSCRIPTION_CHANGED),
                 null, null);
@@ -146,7 +146,7 @@ public class GsmCellBroadcastHandler extends CellBroadcastHandler {
         super("GsmCellBroadcastHandler", context, looper, cbSendMessageCalculatorFactory,
                 handlerHelper);
         mContext.registerReceiver(mGsmReceiver, new IntentFilter(ACTION_AREA_UPDATE_ENABLED),
-                CBR_MODULE_PERMISSION, null, Context.RECEIVER_NOT_EXPORTED);
+                CBR_MODULE_PERMISSION, null, RECEIVER_EXPORTED);
         mContext.registerReceiver(mGsmReceiver,
                 new IntentFilter(SubscriptionManager.ACTION_DEFAULT_SUBSCRIPTION_CHANGED),
                 null, null);
